db34aac597aff94330582f54a90f9f9ffeeba04b
zhaozhiguang <zhaozhiguang@hqjy.com>
2019-09-16 16:12:38 +0800

    security配置

--- src/main/java/com/zhaozhiguang/component/admin/config/SecurityConfig.java
+++ src/main/java/com/zhaozhiguang/component/admin/config/SecurityConfig.java
@@ -1,12 +1,32 @@
 package com.zhaozhiguang.component.admin.config;
 
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@EnableWebSecurity
+@EnableWebSecurity
 //@EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public SecurityConfig() {
+        super(true);//禁用默认配置
+    }
 
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.authorizeRequests()
+                .anyRequest().authenticated().and()
+                .exceptionHandling().and()
+                .headers().and()
+                .securityContext().and()
+                .anonymous().and()
+                .csrf().disable()
+                //.formLogin().and()
+                .httpBasic();
+
+        //http.removeConfigurer(FormLoginConfigurer.class);
+    }
 }
